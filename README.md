271425f0a368d574b2df53904de28d34ea57833f
jvde.github <jvde.github@gmail.com>
2023-02-20 19:30:39 +0100

    add a few settings

--- README.md
+++ README.md
@@ -1,20 +1,50 @@
 # ais2adsb
 
+This  Python script converts AIS nmea lines to BaseStation format so it can be read by programs like Virtual Radar Server (VRS). Main purpose is to plot data on SAR aircraft picked up by AIS receivers in ADSB plotting software.  The program reads AIS messages coming in as NMEA lines over UDP and sends out ADS-B messages in BaseStation [format](http://woodair.net/sbs/article/barebones42_socket_data.htm) to a server. 
 
-This simple python scripts converts AIS nmea lines to BaseStation format so it can be read by programs like Virtual Radar Server (VRS). Main purpose is to plot data on SAR aircraft picked up by AIS receivers in ADSB plotting software.
+The following is an example of a SAR helicopter broadcasting AIS messages plotted in Virtual Radar Server where reception of AIS is better (courtesy of jonboy1081):
 
-To use, you need to install Python3 and pyais (if not already installed):
+And plotted using the ADS-B data:
+
+
+## Usage
+```
+Usage: (python) ais2adsb.py <AIS UDP address> <AIS UDP port> <BS server address> <BS server port> <options>
+Options:
+	FILE xxxx        : read mmsi <-> ICAO mapping from file xxxx
+	SHIPS on/off     : include ships in sendout
+	CALLSIGN on/off  : include generated callsigns in sendout
+	PRINT on/off     : print mmsi/ICAO dictionary
+
+```
+This is the minimal command line:
+```
+python ./ais2adsb.py 192.168.1.235 4002 192.168.1.239 30003
+```
+which reads AIS messages coming in on a computer with IP address `192.168.1.235` and port 4002 and sends it to a Virtual Radar Server running at `192.168.1.239` port 30003. Below some more instructions to set up.
+
+There are only a few options. The `FILE` setting will read in a file with a Python Dictionary that maps MMSI numbers to 24-bit ICAO numbers. The Dictionary functionality allows the user to let the program use a pre-defined mapping.  If not provided AIS2ADSB will auto generate ICAO numbers of the form `FXXXXX`  based on the MMSI number or from a default dictionary embedded in the program (courtesy of jonboy1081). The `PRINT on` option will trigger dumping the Dictionary to stderr periodically (so it can be put back in via the FILE option if desired)
+
+The SHIPS setting (on/off, default is off) will instruct the program to also include vessels in the sendout. By default only SAR Aircraft broadcasting AIS message type 9 are included. A callsign based on MMSI will be included by default, unless the option `CALLSIGN off` is given. A full example is:
+```
+python ./ais2adsb.py 192.168.1.235 4002 192.168.1.239 30003 SHIPS on FILE mapping.dict PRINT on CALLSIGN off
+```
+
+## Installation
+
+For Windows users who do not have Python installed there is a package available in the Release sections created via [pyinstaller](https://pyinstaller.org/en/stable/). 
+In general simplest is to install Python3 and pyais (if not already installed):
 ```
 sudo apt install python3
 pip install pyais
 ```
-Then download the package and enter the directory:
+Then download this package and enter the directory:
 ```
 git clone https://github.com/jvde-github/ais2adsb.git
 cd ais2adsb
 ```
 
-Set up VRS so that it can receive BaseStation as a TCP server:
+Set up for example VRS so that it can receive BaseStation messages as a TCP server:
 <img width="659" alt="image" src="https://user-images.githubusercontent.com/52420030/219872223-2d199476-94e4-467c-9943-3cab66e48c4a.png">
 
 The NMEA input should be send over UDP. Most AIS software including AIS-catcher can easily be set up to achieve this. For now we will assume you will have a stream of messages send to the local computer (say `192.168.1.235` at port `4002`). To create BaseStation messages and send to the server use the following command:
@@ -33,6 +63,6 @@ You will see in the VRS main window that the client has connected and hopefully 
 Illustration of plotting ships in VRS:
 <img width="1232" alt="image" src="https://user-images.githubusercontent.com/52420030/219868349-5b1dc1e5-33b1-48a0-96a4-9ad4bb49134f.png">
 
-To do:
+## To do
 - Fine tuning the Basestation messages (as I learn more about ADSB and the software)
-- A lot....
+- ....
--- ais2adsb.py
+++ ais2adsb.py
@@ -4,55 +4,29 @@ import socket
 import signal
 import pyais
 from pyais import decode
+import ast
 import time
 from datetime import datetime
 import sys
 
-ICAOmap = { 111232512:0x406C79,
-            111232511:0x406C82,
-            111232513:0x406C8E,
-            111232516:0x406D2C,
-            111232517:0x406D2D,
-            111232523:0x406DDB,
-            111232524:0x406DDC,
-            111232529:0x406F8B,
-            111232526:0x406EE7,
-            111232528:0x406F2D,
-            111232518:0x406D21,
-            111232533:0x406DE5,
-            111232522:0x406DE6,
-            111232527:0x406DE7,
-            111232525:0x406DE8,
-            111232534:0x406DE9,
-            111232535:0x406DEA,
-            111232537:0x406DEB,
-            111232539:0x406DED,
-            111232531:0x43ECF4,
-            250002898:0x4CA98D,
-            250002897:0x4CA98F,
-            250002902:0x4CA98B,
-            250004879:0x4CACA4,
-            250002901:0x4CA98C,
-            111232519:0x48644B,
-            111232538:0x485F8F,
-            111503003:0x4860B1,
-            111232509:0x47BFE4
-}
+ICAOmap = { 111232512:0x406C79, 111232511:0x406C82, 111232513:0x406C8E, 111232516:0x406D2C, 111232517:0x406D2D, 111232523:0x406DDB, 111232524:0x406DDC, 111232529:0x406F8B, 111232526:0x406EE7,
+            111232528:0x406F2D, 111232518:0x406D21, 111232533:0x406DE5, 111232522:0x406DE6, 111232527:0x406DE7, 111232525:0x406DE8, 111232534:0x406DE9, 111232535:0x406DEA,111232537:0x406DEB,
+            111232539:0x406DED, 111232531:0x43ECF4, 250002898:0x4CA98D, 250002897:0x4CA98F, 250002902:0x4CA98B, 250004879:0x4CACA4, 250002901:0x4CA98C, 111232519:0x48644B,111232538:0x485F8F,
+            111503003:0x4860B1, 111232509:0x47BFE4 }
+
+settings = { "SERVER_IP":"", "SERVER_PORT": 0, "UDP_IP":"" , "UDP_PORT":0, "includeShips":False, "includeCallSign": True, "printDict": False }
 
 client_socket = None
-SERVER_IP = "192.168.1.235"
-SERVER_PORT = 30003
 sent = 0
 
 def generateICAO(mmsi):
     global ICAOmap, maxICAO
     if mmsi not in ICAOmap:
         proposedICAO = 0xF00000 | (mmsi & 0xFFFFF)
-        ICAO = '%X' % proposedICAO
-        print(f'New mmsi: {mmsi}, proposed ICAO: {ICAO}', file=sys.stderr)
+        print(f'New mmsi: {mmsi}, generated ICAO: {"%X" % proposedICAO}', file=sys.stderr)
         if proposedICAO in ICAOmap.values():
             while True:
-                print(f'ICAO already taken, skipping', file=sys.stderr)
+                print(f'ICAO occupied, skipping to next', file=sys.stderr)
                 proposedICAO = (proposedICAO + 1) & 0xFFFFFF
                 if proposedICAO not in ICAOmap.values():
                     break
@@ -62,21 +36,52 @@ def generateICAO(mmsi):
 
 def connectClient():
     global client_socket
-    global SERVER_IP, SERVER_PORT
+    global settings
 
     while True:
         try:
             client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            client_socket.connect((SERVER_IP, SERVER_PORT))
+            client_socket.connect((settings["SERVER_IP"], settings["SERVER_PORT"]))
             break
         except (socket.error, OSError) as e:
             print(f"Error: failed to connect to server: error code {e}",file=sys.stderr)
             time.sleep(10)
             continue
 
-    print(f"AIS2ADSB: Connected to ADSB server",file=sys.stderr)
+    print(f"Status: Connected to ADSB server",file=sys.stderr)
+
+def loadMMSIdict(str):
+    
+    print(f'Reading dictionary from MMSI to ICAO from file "{str}"')
+
+    with open(str) as f:
+        data = f.read()
+        d = ast.literal_eval(data)
+  
+        global ICAOmap
+
+        for key in d:
+            if key in ICAOmap and ICAOmap[key] != d[key]:                
+                print(f'\tWarning: overwrite {key} -> {"%X" % ICAOmap[key]}',file=sys.stderr)
+            ICAOmap[key] =  d[key]
+    f.close()
+
+def printDictionary():
+    print("{", end="",file=sys.stderr)
+    first = True
+    for key in ICAOmap:
+        if first: 
+            first = False
+        else:
+            print(",", end="",file=sys.stderr)
+
+        print(f"{key}:0x{'%X'%ICAOmap[key]}", end="",file=sys.stderr)
+
+    print("}",file=sys.stderr)
 
 def sendBaseStation(decoded):
+
+    global settings
 
     alt = decoded.get('alt', 0)
     lat = decoded.get('lat', None)
@@ -84,16 +89,13 @@ def sendBaseStation(decoded):
     speed = decoded.get('speed', None)
     heading = decoded.get('course', None)
 
-    #s1 = f'MSG,3,1,0,{ICAO},1,{dstr},{tstr},{dstr},{tstr},,{alt},,,{lat},{lon},,,0,0,0,0\n'
-    #s2 = f'MSG,4,1,0,{ICAO},1,{dstr},{tstr},{dstr},{tstr},,,{speed},{heading},,,0,,,,,\n'
-
     if lat != None and lon != None and speed != None and heading != None:
         ICAO = '%X' % generateICAO(decoded['mmsi'])
 
         now_utc = datetime.now()
         dstr = now_utc.strftime("%Y/%m/%d")
         tstr = now_utc.strftime("%H:%M:%S.%f")[:-3]
-        callsign = "AIS" + ICAO[4:]
+        callsign = "V:" + ("00000" + str(decoded['mmsi']) )[-6:]
 
         global client_socket
 
@@ -106,7 +108,8 @@ def sendBaseStation(decoded):
         else:
             try:         
                 client_socket.send(spos.encode())
-                client_socket.send(scs.encode())
+                if settings["includeCallSign"]:
+                    client_socket.send(scs.encode())
 
                 global sent
                 sent = sent + 1
@@ -119,39 +122,83 @@ def sendBaseStation(decoded):
             
 # this should be fixed so we properly close the sockets etc....
 
+def printUsage():
+    print("Usage: (python) ais2adsb.py <AIS UDP address> <AIS UDP port> <BS server address> <BS server port> <options>")
+    print("Options:")
+    print("\tFILE xxxx        : read mmsi <-> ICAO mapping from file xxxx")
+    print("\tSHIPS on/off     : include ships in sendout")
+    print("\tCALLSIGN on/off  : include generated callsigns in sendout")
+    print("\tPRINT on/off     : print mmsi/ICAO dictionary")
+
 def signalHandler(sig, frame):
     print('Ctrl+C pressed terminating')
     sys.exit(0)
 
+def parseSwitch(str):
+    if str.upper() == "ON": 
+        return True
+    elif str.upper() == "OFF": 
+        return False
+
+    raise Exception("Unknown switch on command line: " + str)
+    
+def parseCommandLine():
+    global UDP_IP, UDP_PORT, SERVER_IP,SERVER_PORT, includeShips, includeCallSign
+
+    if len(sys.argv) < 5:
+        if len(sys.argv) > 1:
+            raise Exception("Command line should at least have 4 parameters")
+    
+        return False
+
+    settings["UDP_IP"] = sys.argv[1]
+    settings["UDP_PORT"] = int(sys.argv[2])
+
+    settings["SERVER_IP"] = sys.argv[3]
+    settings["SERVER_PORT"] = int(sys.argv[4])
+
+    if len(sys.argv) >= 6:
+        if len(sys.argv) >= 7 and len(sys.argv) % 2 == 1:
+            for i in range(5,  len(sys.argv),2):   
+                opt = sys.argv[i].upper()
+                if opt == 'SHIPS':
+                    settings["includeShips"] = parseSwitch(sys.argv[i+1])
+                elif opt == 'PRINT':
+                    settings["printDict"] = parseSwitch(sys.argv[i+1])                    
+                elif opt == 'FILE':
+                    loadMMSIdict(sys.argv[i+1])
+                elif opt == 'CALLSIGN':
+                    settings["includeCallSign"] = parseSwitch(sys.argv[i+1])
+                else:
+                    raise Exception("Unknown option on command line: " + opt)
+        else:
+            raise Exception("Command line options should be in key/value pairs")
+
+    return True
+
 
 signal.signal(signal.SIGINT, signalHandler)
 
-print(f"AIS2ADSB v0.1 - see https://github.com/jvde-github/ais2adsb", file=sys.stderr)
+print(f"AIS2ADSB v0.11 - see https://github.com/jvde-github/ais2adsb", file=sys.stderr)
 
-if len(sys.argv) < 5:
-    print("Usage: python ais2adsb.py <AIS UDP address> <AIS UDP port> <BS server address> <BS server port> <include ships if not empty>")
-    sys.exit(0)
+try:
+    if not parseCommandLine():
+        printUsage()
+        sys.exit(0)
+except Exception as e:
+    print(e)
+    sys.exit(1)
 
-
-UDP_IP = sys.argv[1]
-UDP_PORT = int(sys.argv[2])
-includeShips = False
-
-SERVER_IP = sys.argv[3]
-SERVER_PORT = int(sys.argv[4])
-
-if len(sys.argv) == 6:
-    includeShips = True
-
-
-print(f"Input AIS     : {UDP_IP}:{UDP_PORT}", file=sys.stderr)
-print(f"Output SBS    : {SERVER_IP}:{SERVER_PORT}", file=sys.stderr)
-print(f"Include ships : {includeShips}", file=sys.stderr)
+print(f"Input AIS        : {settings['UDP_IP']}:{settings['UDP_PORT']}", file=sys.stderr)
+print(f"Output SBS       : {settings['SERVER_IP']}:{settings['SERVER_PORT']}", file=sys.stderr)
+print(f"Include ships    : {settings['includeShips']}", file=sys.stderr)
+print(f"Include callsign : {settings['includeCallSign']}", file=sys.stderr)
+print(f"Print Dictionary : {settings['printDict']}", file=sys.stderr)
 
 connectClient()
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind((UDP_IP, UDP_PORT))
+sock.bind((settings['UDP_IP'], settings['UDP_PORT']))
 
 next_update_time = time.monotonic() + 10
 count  = 0
@@ -167,13 +214,16 @@ while True:
     except pyais.exceptions.MissingMultipartMessageException as e:
         continue
 
-    if decoded['msg_type']==9 or includeShips or (decoded['mmsi'] in ICAOmap):
+    if decoded['msg_type']==9 or settings['includeShips'] or (decoded['mmsi'] in ICAOmap):
         sendBaseStation(decoded)
         count = count + 1
 
     if time.monotonic() >= next_update_time:
         t = time.strftime('%Y-%m-%d %H:%M:%S')
         print(f'{t} Messages sent: {sent}/{count}')
+        if settings['printDict']:
+            printDictionary()
+
         count = 0
         sent = 0
         next_update_time += 10
